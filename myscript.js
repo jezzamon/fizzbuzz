404f2cb2533952abc2d88639da72260c527038dd
jezzamon <jerrelunia@gmail.com>
2014-06-29 13:33:35 -0400

    added reset button

--- myscript.js
+++ myscript.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
+	
 
-$('form').submit(function() {
+$('#form').submit(function() {
 			event.preventDefault();
 			var startNumber = +$("#number").val();
 			fizzBuzz(startNumber);
@@ -10,20 +11,25 @@ $('form').submit(function() {
 			});		
 
 var fizzBuzz= function(number) {
+
+	if (number >100) {
+		alert("You must enter a number between 1 and 100")
+	};
 	
 	for (var i = 0 ; i <= number; i++) {
 			if (i % 3 === 0 && i % 5 == 0) {
 				$(".result").append("<span> fizzbuzz!! </span>");
-			} else if (i % 5 === 0) {
-				$(".result").append("<span> buzz!! </span>");
-			} else if (i % 3 == 0 ) {
-				 $(".result").append("<span> fizz!! </span>");
-			} else {
-				$(".result").append("<span>  " + i + " </span>");
-				}		
-	}
-}
+					} else if (i % 5 === 0) {
+						$(".result").append("<span> buzz!! </span>");
+							} else if (i % 3 == 0 ) {
+				 				$(".result").append("<span> fizz!! </span>");
+									} else {
+										$(".result").append("<span>  " + i + " </span>");
+										}		
+									}
+								}
+$(document).on("click", "#reset", function() {
+	$("span").remove();
+	});
 
-
-    
-});
+							});
