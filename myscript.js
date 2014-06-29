f8d27d506a4afd184a3bf5345f9d22ed909b0e7b
Grae Drake <graemdrake@gmail.com>
2014-06-29 14:38:54 -0400

    Add comments and questions

--- myscript.js
+++ myscript.js
@@ -1,17 +1,27 @@
 $(document).ready(function() {
 	
+	// Event handler for submitting the form (e.g. clicking the submit button).
 	$('#form').submit(function() {
+		// By default form submission refreshes the page.
 		event.preventDefault();
+		// Grab the starting number from the form input.
 		var startNumber = +$("#number").val();
 		fizzBuzz(startNumber);
 	});
 
+	// Function to determine how to treat each number.
 	var fizzBuzz= function(number) {
 
+		// Send an alert fizzBuzz is called with a number over 100.
+		// Does this error checking belong inside this function?
+		// If so, looks like we may want an else clause here.
 		if (number >100) {
 			alert("You must enter a number between 1 and 100");
 		}
-	
+		
+		// The meat of the fizzBuzz function.
+		// Looks good, but you might notice that the game always prints "fizzbuzz"
+		// for the first number.  Why do you think that is?
 		for (var i = 0 ; i <= number; i++) {
 			if (i % 3 === 0 && i % 5 === 0) {
 				$(".result").append("<span> fizzbuzz!! </span>");
@@ -25,6 +35,8 @@ $(document).ready(function() {
 		}
 	};
 
+	// Event handler for clicking the reset button.
+	// This function removes *all* spans from the document.  Is that what you want?
 	$(document).on("click", "#reset", function() {
 		$("span").remove();
 	});
