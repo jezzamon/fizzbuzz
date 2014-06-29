7afe1d1dda0f3582fda269fac6936f74b79240b0
Grae Drake <graemdrake@gmail.com>
2014-06-29 14:25:06 -0400

    Fix formatting, semicolons, and === operators

--- myscript.js
+++ myscript.js
@@ -1,35 +1,31 @@
 $(document).ready(function() {
 	
-
-$('#form').submit(function() {
-			event.preventDefault();
-			var startNumber = +$("#number").val();
-			fizzBuzz(startNumber);
-				/*if (startNumber != Math.floor(startNumber)) {
-					alert=("Whole numbers only");
-				} */
-			});		
-
-var fizzBuzz= function(number) {
-
-	if (number >100) {
-		alert("You must enter a number between 1 and 100")
-	};
-	
-	for (var i = 0 ; i <= number; i++) {
-			if (i % 3 === 0 && i % 5 == 0) {
-				$(".result").append("<span> fizzbuzz!! </span>");
-					} else if (i % 5 === 0) {
-						$(".result").append("<span> buzz!! </span>");
-							} else if (i % 3 == 0 ) {
-				 				$(".result").append("<span> fizz!! </span>");
-									} else {
-										$(".result").append("<span>  " + i + " </span>");
-										}		
-									}
-								}
-$(document).on("click", "#reset", function() {
-	$("span").remove();
+	$('#form').submit(function() {
+		event.preventDefault();
+		var startNumber = +$("#number").val();
+		fizzBuzz(startNumber);
 	});
 
-							});
+	var fizzBuzz= function(number) {
+
+		if (number >100) {
+			alert("You must enter a number between 1 and 100");
+		}
+	
+		for (var i = 0 ; i <= number; i++) {
+			if (i % 3 === 0 && i % 5 === 0) {
+				$(".result").append("<span> fizzbuzz!! </span>");
+			} else if (i % 5 === 0) {
+				$(".result").append("<span> buzz!! </span>");
+			} else if (i % 3 === 0 ) {
+				$(".result").append("<span> fizz!! </span>");
+			} else {
+				$(".result").append("<span>  " + i + " </span>");
+			}
+		}
+	};
+
+	$(document).on("click", "#reset", function() {
+		$("span").remove();
+	});
+});
