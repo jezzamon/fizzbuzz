8525da4e26adf588f7bfe4a99fadcae53e73f922
jezzamon <jerrelunia@gmail.com>
2014-06-28 20:10:47 -0400

    Update
    Javascript added

--- myscript.js
+++ myscript.js
@@ -1,22 +1,29 @@
 $(document).ready(function() {
 
-for (var i = 0 ; i <= 100; i++) {
-	$(".result").append("<span> the number " + i + "</span><br>");
-	console.log(i);
-	if (i % 3 === 0) {
-			console.log("fizz!");
-			$(".result").append("<span> fizz!! </span>");
-		}
-		if (i % 5 === 0) {
-			console.log("buzz!");
-			$(".result").append("<span> buzz!! </span>");
+$('form').submit(function() {
+			event.preventDefault();
+			var startNumber = +$("#number").val();
+			fizzBuzz(startNumber);
+				/*if (startNumber != Math.floor(startNumber)) {
+					alert=("Whole numbers only");
+				} */
+			});		
+
+var fizzBuzz= function(number) {
+	
+	for (var i = 0 ; i <= number; i++) {
+			if (i % 3 === 0 && i % 5 == 0) {
+				$(".result").append("<span> fizzbuzz!! </span>");
+			} else if (i % 5 === 0) {
+				$(".result").append("<span> buzz!! </span>");
+			} else if (i % 3 == 0 ) {
+				 $(".result").append("<span> fizz!! </span>");
+			} else {
+				$(".result").append("<span>  " + i + " </span>");
+				}		
 	}
-		if (i % 3 === 0) {
-			if (i % 5 === 0) {
-				console.log("fizzbuzz!");
-			$(".result").append("<span> fizzbuzz!! </span>")
-			}
-		}
-	}
-		
+}
+
+
+    
 });
